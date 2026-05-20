1f80536014ccbe55bc0e4e3a9cd7529a0d40fe7e
Aminat Adebayo <adebayo.aminat@gmail.com>
2026-05-20 11:54:59 -0400

    updates

--- age.py
+++ age.py
@@ -1,7 +1,5 @@
-age_years = input( "How mnay years old are you? ")
+age_years = int(input( "How mnay years old are you? "))
 
-years = int(age_years)
-
-months = years * 12
+months = age_years * 12
 
 print(f"You are {months} months old.")
